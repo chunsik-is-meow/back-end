7054a0763e37d21310aa895e41adc2039923d27b
djagydms <mnbbang2@naver.com>
2021-09-12 21:40:58 +0900

    feature: get table data & change key:value

--- app/controllers/chaincode.controller.js
+++ app/controllers/chaincode.controller.js
@@ -39,10 +39,9 @@ exports.model = async (req, res) => {
 
 exports.allmodel = async (req, res) => {
   var org = 'management.pusan.ac.kr';
-  const result = await Chaincode.query(org, 'ai-model', 'ai-model', 'GetAllAIModelInfo');
-  let aimodel = [];
-  for(var i in result){
-    aimodel[i] = JSON.parse(result[i]);
+  const result = await Chaincode.query(org, 'ai-model', 'ai-model', ['GetAllAIModelInfo']);
+  let aimodel = JSON.parse(result);
+  for(var i in aimodel){
     aimodel[i]['num']=parseInt(i)+1;
     delete aimodel[i].Contents;
     delete aimodel[i].type;
@@ -61,8 +60,14 @@ exports.alldata = async (req, res) => {
   else if (req.role == "verifier") {
     org = "verification-01.pusan.ac.kr"
   }
-  const result = await Chaincode.query(org, 'data', "data", 'GetAllCommonDataInfo');
-  //TODO 처리..
-  res.status(200).send(result);
+  const result = await Chaincode.query(org, 'data', "data", ['GetAllCommonDataInfo']);
+  let data = JSON.parse(result);
+  for(i in data){
+    data[i]['num']=parseInt(i)+1;
+    data[i]['extensions']='csv';
+    delete data[i].Contents;
+    delete data[i].type;
+  }
+  res.status(200).send(data);
 };
 
